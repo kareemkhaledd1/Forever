833610f3528b69046da8629aabef904fdec8e69b
kareemkhaled <123971567+kareemkhaledd1@users.noreply.github.com>
2024-09-09 17:34:57 +0300

    eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,14 +1,13 @@
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import react from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-// Flat config format for ESLint
-export default [
+export default tseslint.config(
+    {ignores: ['dist']},
     {
-        ignores: ['dist'],
-    },
-    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
         files: ['**/*.{ts,tsx}'],
         languageOptions: {
             ecmaVersion: 2020,
@@ -17,16 +16,13 @@ export default [
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
-            'react': react,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
-            ...react.configs.recommended.rules,
             'react-refresh/only-export-components': [
                 'warn',
                 {allowConstantExport: true},
             ],
-            // '@typescript-eslint/no-unused-vars': 'error', // TypeScript rule
         },
     },
-];
+)
